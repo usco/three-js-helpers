4d34407a80ccbbe37510060692e95b9b4b114ccb
Mark Moissette <kaosat.dev@gmail.com>
2014-10-24 14:17:48 +0200

    - refactored DimensionsHelper:  * added attach/detach methods so that it behaves somewhat similarly to three.js transform controls  * it is now not meant to be added as a child of the object whose dimensions it displays, and can be kept in a seperate scene  * refactored the code accordingly to the points above (ie moved most of the code from the constructor to the "attach" method)

--- dimensions/DimensionsHelper.js
+++ dimensions/DimensionsHelper.js
@@ -2,28 +2,38 @@
 ObjectDimensionsHelper = function (options) {
   BaseHelper.call( this );
   var options = options || {};
-  var color = options.color || 0x000000;
+  var color = this.color = options.color || 0x000000;
   var mesh = options.mesh || this.parent || null;
 
-  var cage = new THREE.Object3D()
+}
+
+ObjectDimensionsHelper.prototype = Object.create( BaseHelper.prototype );
+ObjectDimensionsHelper.prototype.constructor = ObjectDimensionsHelper;
+
+
+ObjectDimensionsHelper.prototype.attach = function(mesh){
+  var color = this.color;
+  var mesh = this.mesh = mesh;
   var lineMat = new THREE.MeshBasicMaterial({color: color, wireframe: true, shading:THREE.FlatShading});
+  var dashMaterial = new THREE.LineDashedMaterial( { color: 0x000000, dashSize: 2.5, gapSize: 2, depthTest: false,linewidth:1} );
+  
+  /*mesh.updateMatrixWorld();
+  var matrixWorld = new THREE.Vector3();
+  matrixWorld.setFromMatrixPosition( mesh.matrixWorld );
+  this.position.copy( matrixWorld );*/
 
   this.getBounds(mesh);
   var delta = this.computeMiddlePoint(mesh);
-  cage.position = delta
-  
+  //var delta = new THREE.Vector3();
   
   var width = this.width;
   var length = this.length;
   var height = this.height;
-  console.log("w",width,"l",length,"h",height);
-
-  var widthArrowPos = new THREE.Vector3(delta.x+this.length/2,delta.y,delta.z-this.height/2); 
-  var lengthArrowPos = new THREE.Vector3( delta.x, delta.y+this.width/2, delta.z-this.height/2)
-  var heightArrowPos = new THREE.Vector3( delta.x-this.length/2,delta.y+this.width/2,delta.z)
-
-  //console.log("width", this.width, "length", this.length, "height", this.height,"delta",delta, "widthArrowPos", widthArrowPos)
-  var dashMaterial = new THREE.LineDashedMaterial( { color: 0x000000, dashSize: 2.5, gapSize: 2, depthTest: false,linewidth:1} )
+  console.log("w",width,"l",length,"h",height,delta);
+  
+  var baseCubeGeom = new THREE.BoxGeometry(this.length, this.width,this.height)
+  this.meshBoundingBox = new THREE.Mesh(baseCubeGeom,new THREE.MeshBasicMaterial({wireframe:true,color:0xff0000}))
+  //this.add( this.meshBoundingBox )
 
   var baseOutlineGeometry = new THREE.Geometry();
   baseOutlineGeometry.vertices.push(new THREE.Vector3(-this.length/2, -this.width/2, 0));
@@ -36,28 +46,37 @@ ObjectDimensionsHelper = function (options) {
   var baseOutline = new THREE.Line( baseOutlineGeometry, dashMaterial, THREE.Lines );
   baseOutline.renderDepth = 1e20
   baseOutline.position.copy( new THREE.Vector3(delta.x,delta.y,delta.z-this.height/2) );
+  this.baseOutline = baseOutline; 
   this.add(baseOutline);
+  
 
-  baseCubeGeom = new THREE.BoxGeometry(this.length, this.width,this.height)
-  var bla = new THREE.Mesh(baseCubeGeom,new THREE.MeshBasicMaterial({wireframe:true,color:0xff0000}))
-  bla.position = new THREE.Vector3(delta.x,delta.y,delta.z);
-  //this.add( bla )
-
+  var widthArrowPos = new THREE.Vector3(delta.x+this.length/2,delta.y,delta.z-this.height/2); 
+  var lengthArrowPos = new THREE.Vector3( delta.x, delta.y+this.width/2, delta.z-this.height/2)
+  var heightArrowPos = new THREE.Vector3( delta.x-this.length/2,delta.y+this.width/2,delta.z)
+  //console.log("width", this.width, "length", this.length, "height", this.height,"delta",delta, "widthArrowPos", widthArrowPos)
   var sideLength =10;
   
   //length, sideLength, position, direction, color, text, textSize,
   var widthArrow  = new SizeHelper( {length:this.width,sideLength:sideLength,position:widthArrowPos,direction:new THREE.Vector3(0,0,-1) });
   var lengthArrow = new SizeHelper( {length:this.length,sideLength:sideLength,position:lengthArrowPos,direction:new THREE.Vector3(1,0,0) });
   var heightArrow = new SizeHelper( {length:this.height,sideLength:sideLength,position:heightArrowPos,direction:new THREE.Vector3(0,1,0) });
-        
-  this.add( widthArrow );
-  this.add( lengthArrow );
-  this.add( heightArrow );
-
+  
+  
+  this.arrows = new THREE.Object3D();
+  this.arrows.add( widthArrow );
+  this.arrows.add( lengthArrow );
+  this.arrows.add( heightArrow );
+  
+  this.add( this.arrows );
 }
 
-ObjectDimensionsHelper.prototype = Object.create( BaseHelper.prototype );
-ObjectDimensionsHelper.prototype.constructor = ObjectDimensionsHelper;
+ObjectDimensionsHelper.prototype.detach = function(mesh){
+  this.mesh = null;
+  
+  //this.remove( this.meshBoundingBox );
+  this.remove( this.baseOutline );
+  this.remove( this.arrows );
+}
 
 ObjectDimensionsHelper.prototype.computeMiddlePoint=function(mesh)
 {
