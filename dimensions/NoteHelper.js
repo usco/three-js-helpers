18d8d66b834e34a7ccc787da8ee613f017e335e3
Mark Moissette <kaosat.dev@gmail.com>
2014-12-17 15:25:13 +0100

    - updated noteHelper to match dynamic internals of other annotations helpers (show/hide visuals instead of recreating them)

--- dimensions/NoteHelper.js
+++ dimensions/NoteHelper.js
@@ -22,46 +22,17 @@ NoteHelper = function(options)
   this.crossColor = options.crossColor!== undefined ? options.crossColor : "#F00";
   this.text       = options.text      !== undefined ? options.text : "";
   this.title      = options.title     !== undefined ? options.title : "";
-  
-  /*
-  var material = new THREE.LineBasicMaterial( { color: 0x000000, depthTest:false,depthWrite:false,renderDepth : 1e20});
 
-  //draw arrow
-  var arrowOffset = new THREE.Vector3(Math.sqrt(this.distance)*2+this.diameter/2,Math.sqrt(this.distance)*2+this.diameter/2,0);
-  var mainArrow = new THREE.ArrowHelper2(new THREE.Vector3(-1,-1,0),new THREE.Vector3(),this.distance , this.color);
-  mainArrow.position.add(arrowOffset);
-
-  var endLineEndPoint = arrowOffset.clone().add( new THREE.Vector3( this.endLength, 0, 0 ) ) ;
-  var endLineGeometry = new THREE.Geometry();
-  endLineGeometry.vertices.push( arrowOffset );
-  endLineGeometry.vertices.push( endLineEndPoint ); 
-   var endLine = new THREE.Line( endLineGeometry, material );
-
-  //draw dimention / text
-  this.label = new LabelHelper3d({text:this.text,fontSize:this.fontSize, color:this.textColor, bgColor:this.textBgColor});
-  this.label.position.add( endLineEndPoint );
-  //TODO: account for size of text instead of these hacks
-  this.label.position.x += 5
-  this.label.position.y -= 2
+  //initialise internal sub objects
+  this.pointCross = new CrossHelper({size:this.centerCrossSize,color:this.crossColor});
+  this.pointCross.hide();
+  this.add( this.pointCross );
   
   
-  //draw main circle
-  var circleRadius = this.diameter/2;
-  var circleShape = new THREE.Shape();
-	circleShape.moveTo( 0, 0 );
-	circleShape.absarc( 0, 0, circleRadius, 0, Math.PI*2, false );
-  var points  = circleShape.createSpacedPointsGeometry( 100 );
-  var diaCircle = new THREE.Line(points, material );
-
-  //add all
-  this.add( diaCircle );
-  this.add( mainArrow );
-  this.add( endLine );
-  this.add( this.label );
-  this.add( centerCross1 );*/
+  this.point      = options.point!== undefined ? options.point : undefined;
+  this.object     = options.object!== undefined ? options.object : undefined;
   
-  this.point = undefined;
-  this.object= undefined;
+  if( options.point ) this.setPoint( this.point, this.object );
 }
 
 NoteHelper.prototype = Object.create( AnnotationHelper.prototype );
@@ -69,17 +40,15 @@ NoteHelper.prototype.constructor = NoteHelper;
 
 
 NoteHelper.prototype.unset = function( ){
-  this.remove( this.pointCross );
+  this.pointCross.hide();
 }
 
 NoteHelper.prototype.setPoint = function( point, object ){
   if(point) this.point = point;
   if(object) this.object = object;
 
-  if(this.pointCross) this.remove( this.pointCross );
   //point location cross
-  this.pointCross = new CrossHelper({size:this.centerCrossSize,color:this.crossColor});
   this.pointCross.position.copy( this.point );
-  this.add( this.pointCross );
+  this.pointCross.show();
 }
 
