78af49fbbc0556e8bfeb7fd1c53675b10fd841b3
Jim Kang <proclaim@gmail.com>
2016-08-29 14:57:29 +0800

    upgrade compatibility after r72

--- src/CrossHelper.js
+++ src/CrossHelper.js
@@ -17,7 +17,7 @@ var CrossHelper = function(options)
   var opacity = this.opacity = options.opacity || 0.8;
   var id = this.innerDia = options.id || 0;
 
-  
+
   var offsetPos = size/2 + id/2;
   //starting point cross
   var startCrossGeometry = new THREE.Geometry();
@@ -25,22 +25,22 @@ var CrossHelper = function(options)
   startCrossGeometry.vertices.push( new THREE.Vector3( 0, -id/2, 0 ) );
   startCrossGeometry.vertices.push( new THREE.Vector3( 0, offsetPos , 0 ) );
   startCrossGeometry.vertices.push( new THREE.Vector3( 0, id/2 , 0 ) );
-  
+
   startCrossGeometry.vertices.push( new THREE.Vector3( -offsetPos, 0, 0 ) );
   startCrossGeometry.vertices.push( new THREE.Vector3( -id/2, 0, 0 ) );
   startCrossGeometry.vertices.push( new THREE.Vector3( offsetPos, 0 , 0 ) );
   startCrossGeometry.vertices.push( new THREE.Vector3( id/2, 0 , 0 ) );
-  
-  this.centerCross = new THREE.Line( startCrossGeometry, new GizmoLineMaterial( { color: this.color, opacity:opacity, transparent:true } ),THREE.LinePieces );
+
+  this.centerCross = new THREE.LineSegments( startCrossGeometry, new GizmoLineMaterial( { color: this.color, opacity:opacity, transparent:true } ) );
   this.centerCross.material.depthTest   = true;
   this.centerCross.material.depthWrite  = true;
   this.centerCross.material.renderDepth = 1e20;
-  
+
   this.add( this.centerCross ) ;
   //this.position.copy( position );
 }
 
 CrossHelper.prototype = Object.create( BaseHelper.prototype );
-CrossHelper.prototype.constructor = CrossHelper;  
+CrossHelper.prototype.constructor = CrossHelper;
 
 module.exports = CrossHelper;
